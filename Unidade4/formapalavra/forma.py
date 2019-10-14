4b551b97de7e5be4dea3d48dfa3a47d00b6855de
Mariane Carvalho <mariane.carvalho@ccc.ufcg.edu.br>
2019-10-14 19:12:49 -0300

    Update forma.py

--- Unidade4/formapalavra/forma.py
+++ Unidade4/formapalavra/forma.py
@@ -1,26 +1,16 @@
-#UFCG
-#Programação 1 - 2019.2
-#Juan Salvador da Silva -119210821
-#Forma Palavra a partir de Outras 3 Palavras
+p1 = input()
+p2 = input()
+p3 = input()
+saida = ''
 
-palavra1 = input()
-palavra2 = input()
-palavra3 = input()
-palavrao = ''
+for i in range(len(p1)):
+    if p1[i] > p2[i]:
+        maior = p1[i]
+    else:
+        maior = p2[i]
+    if p3[i] > maior:
+        maior= p3[i]
+    
+    saida += maior
 
-
-for letra in range(len(palavra1)):
-    if palavra1[letra] >= palavra2[letra] and palavra2[letra] >= palavra3[letra]:
-        palavrao += palavra1[letra]
-    elif palavra1[letra] >= palavra3[letra] and palavra3[letra] >= palavra2[letra]:
-        palavrao += palavra1[letra]
-    elif palavra2[letra] >= palavra1[letra] and palavra1[letra] >= palavra3[letra]:
-       palavrao += palavra2[letra]
-    elif palavra2[letra] >= palavra3[letra] and palavra3[letra] >= palavra1[letra]:
-       palavrao += palavra2[letra]
-    elif palavra3[letra] >= palavra1[letra] and palavra1[letra] >= palavra2[letra]:
-       palavrao += palavra3[letra]
-    elif palavra3[letra] >= palavra2[letra] and palavra2[letra] >= palavra1[letra]:
-       palavrao += palavra3[letra]
-
-print(palavrao)
+print(saida)
