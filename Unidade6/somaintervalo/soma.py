69bef2cf2dc5e37ebe04a71411bbb02c9a748e5a
Pedro-Manoel <pedromanoel591@gmail.com>
2019-10-20 16:19:17 -0300

    refactoring

--- Unidade6/somaintervalo/soma.py
+++ Unidade6/somaintervalo/soma.py
@@ -2,6 +2,6 @@ def soma_intervalo(a,b):
     soma = 0
     for i in range(a,b+1,1):
         soma += i
-    return soma
+    return soma    
 assert soma_intervalo(5,15) == 110
 assert soma_intervalo(10,10) == 10
